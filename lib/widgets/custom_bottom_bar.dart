3bf20993f706783a5e87018d5786be168aee8417
ntua-el20160 <147728876+ntua-el20160@users.noreply.github.com>
2024-01-10 17:16:09 +0200

    Add files via upload

---
+++ lib/widgets/custom_bottom_bar.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:moodj2/core/app_export.dart';
+
+class CustomBottomBar extends StatefulWidget {
+  CustomBottomBar({this.onChanged});
+
+  Function(BottomBarEnum)? onChanged;
+
+  @override
+  CustomBottomBarState createState() => CustomBottomBarState();
+}
+
+class CustomBottomBarState extends State<CustomBottomBar> {
+  int selectedIndex = 0;
+
+  List<BottomMenuModel> bottomMenuList = [
+    BottomMenuModel(
+      icon: ImageConstant.imgHomeGray50,
+      activeIcon: ImageConstant.imgHomeGray50,
+      type: BottomBarEnum.Homegray50,
+    ),
+    BottomMenuModel(
+      icon: ImageConstant.imgVectorGray50,
+      activeIcon: ImageConstant.imgVectorGray50,
+      type: BottomBarEnum.Vectorgray50,
+    ),
+    BottomMenuModel(
+      icon: ImageConstant.imgTicketGray50,
+      activeIcon: ImageConstant.imgTicketGray50,
+      type: BottomBarEnum.Ticketgray50,
+    ),
+    BottomMenuModel(
+      icon: ImageConstant.imgLock,
+      activeIcon: ImageConstant.imgLock,
+      type: BottomBarEnum.Lock,
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70.v,
+      decoration: BoxDecoration(
+        color: appTheme.black900,
+      ),
+      child: BottomNavigationBar(
+        backgroundColor: Colors.transparent,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedFontSize: 0,
+        elevation: 0,
+        currentIndex: selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        items: List.generate(bottomMenuList.length, (index) {
+          return BottomNavigationBarItem(
+            icon: CustomImageView(
+              imagePath: bottomMenuList[index].icon,
+              height: 44.v,
+              width: 27.h,
+              color: appTheme.gray50,
+            ),
+            activeIcon: CustomImageView(
+              imagePath: bottomMenuList[index].activeIcon,
+              height: 37.v,
+              width: 27.h,
+              color: appTheme.gray50,
+            ),
+            label: '',
+          );
+        }),
+        onTap: (index) {
+          selectedIndex = index;
+          widget.onChanged?.call(bottomMenuList[index].type);
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
+
+enum BottomBarEnum {
+  Homegray50,
+  Vectorgray50,
+  Ticketgray50,
+  Lock,
+}
+
+class BottomMenuModel {
+  BottomMenuModel({
+    required this.icon,
+    required this.activeIcon,
+    required this.type,
+  });
+
+  String icon;
+
+  String activeIcon;
+
+  BottomBarEnum type;
+}
+
+class DefaultWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.all(10),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Please replace the respective Widget here',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
