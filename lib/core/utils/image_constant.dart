1cdc85a01e0f2b5f46876d25cd0aedda65262db0
ntua-el20160 <147728876+ntua-el20160@users.noreply.github.com>
2024-01-10 17:10:26 +0200

    Add files via upload

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,71 @@
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+  // Start_Page images
+  static String imgAndroidLarge = '$imagePath/img_android_large.png';
+
+  // Android Large - EightyEight images
+  static String imgRectangle6 = '$imagePath/img_rectangle_6.svg';
+
+  // Booth-Choose Song images
+  static String imgRectangle150 = '$imagePath/img_rectangle_150.png';
+
+  static String imgInbox = '$imagePath/img_inbox.svg';
+
+  static String imgClock = '$imagePath/img_clock.svg';
+
+  // Booth-Song Search images
+  static String imgClockGray50 = '$imagePath/img_clock_gray_50.svg';
+
+  // Booth-Song Recommendation images
+  static String imgAndroidLarge640x359 =
+      '$imagePath/img_android_large_640x359.png';
+
+  static String imgRewind = '$imagePath/img_rewind.svg';
+
+  // Alternative: Record SoundOne images
+  static String imgContrastBlack900 = '$imagePath/img_contrast_black_900.svg';
+
+  // Post images
+  static String imgRectangle42 = '$imagePath/img_rectangle_42.svg';
+
+  // Common images
+  static String imgImage1 = '$imagePath/img_image_1.png';
+
+  static String imgQuestion = '$imagePath/img_question.svg';
+
+  static String imgRectangle47 = '$imagePath/img_rectangle_47.png';
+
+  static String imgVector = '$imagePath/img_vector.svg';
+
+  static String imgHome = '$imagePath/img_home.svg';
+
+  static String imgVectorGray50 = '$imagePath/img_vector_gray_50.svg';
+
+  static String imgTicket = '$imagePath/img_ticket.svg';
+
+  static String imgLock = '$imagePath/img_lock.svg';
+
+  static String imgHomeGray50 = '$imagePath/img_home_gray_50.svg';
+
+  static String imgVectorGray5044x27 =
+      '$imagePath/img_vector_gray_50_44x27.svg';
+
+  static String imgQuestionOnprimarycontainer =
+      '$imagePath/img_question_onprimarycontainer.svg';
+
+  static String imgPhPlayFill = '$imagePath/img_ph_play_fill.svg';
+
+  static String imgContrast = '$imagePath/img_contrast.svg';
+
+  static String imgUser = '$imagePath/img_user.svg';
+
+  static String imgGroup = '$imagePath/img_group.svg';
+
+  static String imgTicketGray50 = '$imagePath/img_ticket_gray_50.svg';
+
+  static String imgLockGray50 = '$imagePath/img_lock_gray_50.svg';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
