6e7ba38e9bfd1ea329906f062e4e76893442948c
ntua-el20160 <147728876+ntua-el20160@users.noreply.github.com>
2024-01-10 17:11:53 +0200

    Add files via upload

---
+++ lib/localization/en_us/en_us_translations.dart
@@ -0,0 +1,127 @@
+final Map<String, String> enUs = {
+  // Start_Page Screen
+  "lbl_log_in": "Log in", "lbl_sign_up": "Sign up",
+
+  // Android Large - EightyEight Screen
+  "lbl_join_moodj": "Join Moodj",
+  "msg_welcome_to_moodj_please":
+      "Welcome to Moodj\nPlease choose a username:\n\n\nNow a password:\n\n\nFinally, your email and we’re done!",
+
+  // Log in Screen
+  "lbl_login2": "Login", "lbl_password": "Password:",
+  "lbl_username": "Username:",
+
+  // Create/Join Team Screen
+  "lbl_create_team": "Create Team",
+  "lbl_join_team": "Join Team",
+  "msg_insert_team_code": "Insert Team Code:",
+  "msg_or_create_a_new": "Or create a new team:",
+
+  // FeedTwo Screen
+  "lbl_george": "     George", "lbl_i_follow_rivers": "    I follow rivers",
+  "msg_so_much_better_than": "so much better than the remix",
+
+  // FeedOne Screen
+  "lbl_levitating": "Levitating", "lbl_mary": "Mary",
+  "msg_the_absolute_party": "The absolute party banger",
+
+  // Feed (Home) Screen
+  "lbl_clocks": "Clocks", "lbl_john": "John",
+  "msg_pure_nostalgia": "Pure nostalgia...",
+
+  // Booth-Choose Song Screen
+  "lbl_daily_booth": "Daily Booth",
+
+  // Booth-Song Search Screen
+  "lbl_21_pilots": "21 pilots",
+  "lbl_aot": "AOT",
+  "lbl_dua_lipa": "Dua Lipa",
+  "lbl_levi": "levi",
+  "lbl_levi_s_theme": "Levi’s Theme",
+  "lbl_levitate": "levitate",
+  "lbl_levitating2": "levitating",
+
+  // Song Recommendation Screen
+  "lbl_proceed": "Proceed",
+  "msg_based_on_your_selection":
+      "Based on your selection, we think you should check out this song:",
+  "msg_moodj_s_recommendation": "Moodj’s Recommendation",
+
+  // Alternative: Record SoundTwo Screen
+  "lbl_saved": "Saved", "lbl_submit": "Submit",
+
+  // Alternative: Record SoundOne Screen
+  "lbl": "....",
+
+  // Booth-Vote for users Screen
+  "lbl_song_1": "Song 1",
+
+  // Vote for usersOne Screen
+  "lbl_song_2": "Song 2",
+
+  // Vote for usersTwo Screen
+  "lbl_song_3": "Song 3",
+
+  // Booth-Game Finished Screen Screen
+  "msg_thanks_for_playing_stay":
+      "Thanks for playing!\n\nStay tuned for the results in the leaderboard and\njoin again tomorrow!",
+
+  // Stats Screen
+  "lbl_local_picks": "Local\nPicks", "lbl_stats": "Stats",
+  "msg_friends_leaderboard": "Friends\n Leaderboard",
+
+  // Booth-Alternative: Record Sound Screen
+  "lbl_record": "Record",
+
+  // Local Picks Screen
+  "lbl_local_picks2": "Local Picks",
+  "msg_for_yesterday_s": "... for yesterday’s challenge",
+
+  // Settings Screen
+  "lbl_change_password": "Change Password",
+  "lbl_change_username": "Change Username",
+  "lbl_leave_team": "Leave Team",
+  "lbl_log_out": "Log out",
+  "lbl_post_note": "Post Note",
+  "msg_account_settings": "Account Settings:",
+  "msg_social_settings": "Social Settings:",
+  "msg_team_number_783": "Team Number: 783",
+
+  // Change Password Screen
+  "msg_enter_your_new_password": "Enter your new password:",
+
+  // Post Screen
+  "lbl_post": "Post", "lbl_write": "Write", "lbl_you": "You",
+  "lbl_your_song": "Your Song",
+
+  // Change Username Screen
+  "msg_enter_your_new_username": "Enter your new username:",
+
+  // Common String
+  "lbl_1": "#1",
+  "lbl_2": "#2",
+  "lbl_3": "#3",
+  "lbl_all_time": "All-time",
+  "lbl_artist": "Artist",
+  "lbl_booth": "Booth ",
+  "lbl_booth2": "Booth",
+  "lbl_feed": "Feed",
+  "lbl_moodj": "Moodj",
+  "lbl_song_name": "Song_name",
+  "lbl_song_name2": "Song Name",
+  "lbl_song_search": "Song Search ",
+  "lbl_update": "Update",
+  "lbl_username2": "Username",
+  "lbl_yesterday_s": "Yesterday’s",
+  "msg_friend_leaderboard": "Friend Leaderboard",
+  "msg_late_night_jogging": "Late night jogging by the city lights",
+  "msg_swipe_for_more": "Swipe for more...",
+
+// Network Error String
+  "msg_network_err": "Network Error",
+  "msg_something_went_wrong": "Something Went Wrong!",
+
+  // Validation Error String
+  "err_msg_please_enter_valid_text": "Please enter valid text",
+  "err_msg_please_enter_valid_password": "Please enter valid password",
+};
